74b6f367b0c824892c4de85f956c438bfb19e623
safak mengu <mengusafak@gmail.com>
2023-03-21 15:44:02 +0000

    fisrt push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+
+  @brands = ["JETON", "The Kingdom Bank", "Financial House", "JetonCash", "GuardianPro","Luqapay","Lilibet","Sultanbet", "AngelsHub"]
+  end
+end
